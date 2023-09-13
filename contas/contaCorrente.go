cee1eb46830d3bbdeb19f8477a0987245e8869a3
Flavio Medeiros <flavio.medeiroso@origoenergia.com.br>
2023-09-13 17:06:58 -0300

    Criado o primeiro modulo para organizacao dos codigos

---
+++ contas/contaCorrente.go
@@ -0,0 +1,41 @@
+package contas
+
+import "strconv"
+
+type ContaCorrente struct {
+	Titular string
+	Saldo   float64
+}
+
+func (conta *ContaCorrente) Sacar(valor float64) string {
+	if valor > conta.Saldo {
+		return "Falha ao realizar saque. Saldo insulficiente"
+	}
+
+	conta.Saldo -= valor
+	return "Saque realizado com sucesso. Novo saldo: " + strconv.FormatFloat(conta.Saldo, 'f', 2, 64)
+}
+
+func (conta *ContaCorrente) Depositar(valor float64) string {
+	if valor < 0 {
+		return "Falha ao depositar. Não é permitido depositar valores negativos"
+	}
+
+	conta.Saldo += valor
+	return "Deposito realizado com sucesso. Novo saldo: " + strconv.FormatFloat(conta.Saldo, 'f', 2, 64)
+}
+
+func (conta *ContaCorrente) Transferir(contaDestino *ContaCorrente, valor float64) string {
+	if valor < 0 {
+		return "Não é permitido transferir valores negativos"
+	}
+
+	if valor > conta.Saldo {
+		return "Falha ao realizar saque. Saldo insulficiente"
+	}
+
+	conta.Saldo -= valor
+	contaDestino.Depositar(valor)
+
+	return "Transferencia realizada com sucesso. Novo saldo: " + strconv.FormatFloat(conta.Saldo, 'f', 2, 64)
+}
--- main.go
+++ main.go
@@ -1,58 +1,20 @@
 package main
 
 import (
+	"banco/contas"
 	"fmt"
-	"strconv"
 )
-
-type ContaCorrente struct {
-	titular string
-	saldo   float64
-}
-
-func (conta *ContaCorrente) Sacar(valor float64) string {
-	if valor > conta.saldo {
-		return "Falha ao realizar saque. Saldo insulficiente"
-	}
-
-	conta.saldo -= valor
-	return "Saque realizado com sucesso. Novo saldo: " + strconv.FormatFloat(conta.saldo, 'f', 2, 64)
-}
-
-func (conta *ContaCorrente) Depositar(valor float64) string {
-	if valor < 0 {
-		return "Falha ao depositar. Não é permitido depositar valores negativos"
-	}
-
-	conta.saldo += valor
-	return "Deposito realizado com sucesso. Novo saldo: " + strconv.FormatFloat(conta.saldo, 'f', 2, 64)
-}
-
-func (conta *ContaCorrente) Transferir(contaDestino *ContaCorrente, valor float64) string {
-	if valor < 0 {
-		return "Não é permitido transferir valores negativos"
-	}
-
-	if valor > conta.saldo {
-		return "Falha ao realizar saque. Saldo insulficiente"
-	}
-
-	conta.saldo -= valor
-	contaDestino.Depositar(valor)
-
-	return "Transferencia realizada com sucesso. Novo saldo: " + strconv.FormatFloat(conta.saldo, 'f', 2, 64)
-}
 
 func main() {
 
-	contaFlavio := ContaCorrente{
-		titular: "Flavio",
-		saldo:   75.45,
+	contaFlavio := contas.ContaCorrente{
+		Titular: "Flavio",
+		Saldo:   75.45,
 	}
 
-	contaBia := ContaCorrente{
-		titular: "Bia",
-		saldo:   456,
+	contaBia := contas.ContaCorrente{
+		Titular: "Bia",
+		Saldo:   456,
 	}
 
 	fmt.Println("contaFlavio", contaFlavio)
