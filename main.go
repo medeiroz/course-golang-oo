59ca4dbd2d575696f5dbc20b01cfe72889a5d4fc
Flavio Medeiros <flavio.medeiroso@origoenergia.com.br>
2023-09-13 15:57:18 -0300

    Criando ponteiro

--- main.go
+++ main.go
@@ -13,10 +13,19 @@ func main() {
 
 	contaFlavio := ContaCorrente{
 		titular:       "Flavio",
-		numeroAgencia: 0054,
+		numeroAgencia: 54,
 		numeroConta:   123456,
 		saldo:         75.45,
 	}
 
-	fmt.Println(contaFlavio)
+	fmt.Println("contaFlavio", contaFlavio)
+
+	var contaBia *ContaCorrente
+	contaBia = new(ContaCorrente)
+	contaBia.titular = "Bia"
+	contaBia.numeroAgencia = 87
+	contaBia.numeroConta = 6543211
+	contaBia.saldo = 125.40
+
+	fmt.Println("contaBia", *contaBia)
 }
